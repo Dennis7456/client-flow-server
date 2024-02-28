5ecd808c729845de94998d5ce6db215157a5bfb7
Dennis7456 <denniskiplangat.dk@gmail.com>
2024-02-28 07:30:23 +0300

    npm build vite Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
